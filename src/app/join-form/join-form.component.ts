9cd3945f4f17c9776a9e1659b4d4fd2abdf79685
a.gida <arkheonos@gmail.com>
2024-04-14 16:42:59 +0200

    start page

--- src/app/join-form/join-form.component.ts
+++ src/app/join-form/join-form.component.ts
@@ -1,12 +1,13 @@
-import {Component} from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-join-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './join-form.component.html',
   styleUrl: './join-form.component.css'
 })

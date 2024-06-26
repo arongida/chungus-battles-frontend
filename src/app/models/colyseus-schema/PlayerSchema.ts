fdb67513fe5424c57eb58f274fc5ef064b9320c8
Áron Gida <aron.gida@bosch.com>
2024-06-26 18:07:11 +0200

    update player schema

--- src/app/models/colyseus-schema/PlayerSchema.ts
+++ src/app/models/colyseus-schema/PlayerSchema.ts
@@ -5,28 +5,83 @@ import { Item } from './ItemSchema';
 export class Player extends Schema {
   @type('number') playerId: number = 0;
   @type('string') name: string = 'name';
-  @type('number') hp: number = 0;
-  @type('number') attack: number = 0;
-  @type('number') gold: number = 0;
+  @type('number') private _hp: number = 0;
+  @type('number') private _attack: number = 0;
+  @type('number') private _gold: number = 0;
   @type('number') xp: number = 0;
-  @type('number') level: number = 0;
+  @type('number') private _level: number = 0;
   @type('string') sessionId: string = 'sessionId';
   @type('number') private _defense: number = 0;
-  @type('number') attackSpeed: number = 0;
+  @type('number') private _attackSpeed: number = 0;
   @type('number') maxXp: number = 0;
   @type('number') round: number = 1;
   @type('number') lives: number = 3;
   @type('number') wins: number = 0;
+  @type('number') private _poisonStack: number = 0;
   @type('string') avatarUrl: string =
     'https://chungus-battles.b-cdn.net/chungus-battles-assets/Portrait_ID_0_Placeholder.png';
   @type([Talent]) talents: ArraySchema<Talent> = new ArraySchema<Talent>();
   @type([Item]) inventory: ArraySchema<Item> = new ArraySchema<Item>();
+
+  get gold(): number {
+    return this._gold;
+  }
+
+  set gold(value: number) {
+    this._gold = value < 0 ? 0 : value;
+  }
+
+  get level(): number {
+    return this._level;
+  }
+
+  set level(value: number) {
+    this._level = value > 5 ? 5 : value;
+  }
+
+  get attackSpeed(): number {
+    return this._attackSpeed;
+  }
+
+  set attackSpeed(value: number) {
+    this._attackSpeed = value < 0.1 ? 0.1 : value;
+  }
+
+  get hp(): number {
+    return this._hp;
+  }
+
+  set hp(value: number) {
+    this._hp = value < 0 ? 0 : value;
+  }
+
+  get attack(): number {
+    return this._attack;
+  }
+
+  set attack(value: number) {
+    this._attack = value < 1 ? 1 : value;
+  }
+
+  get poisonStack(): number {
+    return this._poisonStack;
+  }
+
+  set poisonStack(value: number) {
+    if (value < 0) {
+      this._poisonStack = 0;
+    } else if (value > 50) {
+      this._poisonStack = 50;
+    } else {
+      this._poisonStack = value;
+    }
+  }
 
   get defense(): number {
     return this._defense;
   }
 
   set defense(value: number) {
-    this._defense = value;
+    this._defense = value < 0 ? 0 : value;
   }
 }

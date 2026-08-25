bc2377765d5690f1a2151b52a7d067ec27211d29
arongida <arkheonos@gmail.com>
2026-08-25 12:05:49 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/arongida/chungus-battles-frontend/commit/a06d11df921955095a00e9c3168a4fba63c4bdd0

---
+++ chunk-DXXRFQF5.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-CM3JUZZ3.js";import"./chunk-5DJWQZSZ.js";import"./chunk-LCR4BXYH.js";import"./chunk-RNXNRFGZ.js";export{a as EncyclopediaComponent};
--- chunk-OFPCFNNB.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-GFNRCW3O.js";import"./chunk-6G6PLDPI.js";export{a as EncyclopediaComponent};
